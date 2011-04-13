c1d63bc7599a2a9658782b93eb3010736be7930f
Jeffrey Hulten <jeffh@automatedlabs.com>
2011-04-13 15:03:24 -0700

    starting work on a netkernel cookbook

---
+++ netkernel/attributes/default.rb
@@ -0,0 +1,4 @@
+default[:netkernel][:version] = "4.1.1"
+default[:netkernel][:install_url] = "http://apposite.netkernel.org/dist/1060-NetKernel-SE/1060-NetKernel-SE-4.1.1.jar"
+
+default[:netkernel][:install_path] = "/opt/netkernel"
---
+++ netkernel/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Automated Labs, LLC"
+maintainer_email "jeffh@automatedlabs.com"
+license          "MIT License"
+description      "Installs/Configures netkernel"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
