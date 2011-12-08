99521ed1b62ffe52be4564f2732d11897b3b3b91
Jeffrey Hulten <jeffh@automatedlabs.com>
2011-12-08 14:07:55 -0600

    added java depends and increment to v 0.1.0

--- netkernel/metadata.rb
+++ netkernel/metadata.rb
@@ -3,6 +3,8 @@ maintainer_email "jeffh@automatedlabs.com"
 license          "MIT License"
 description      "Installs/Configures netkernel"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.2"
+version          "0.1.0"
+
+depends "java"
 
 supports 'ubuntu', ">= 11.04"
