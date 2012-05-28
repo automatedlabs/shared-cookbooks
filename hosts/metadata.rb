98b18a6a93b95046a1ca02db43b0163315230245
hanskreuger <luke.dolman@gmail.com>
2012-05-29 00:43:02 +0200

    corrected README.rdoc to README.md, to stop librarian-chef choking on install

--- hosts/metadata.rb
+++ hosts/metadata.rb
@@ -2,5 +2,5 @@ maintainer       "Jeffrey Hulten"
 maintainer_email "jeffh@automatedlabs.com"
 license          "MIT Licensed"
 description      "Installs/Configures hosts"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.1"
